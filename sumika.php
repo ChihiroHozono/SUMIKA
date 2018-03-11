f8e6796228d281ccd730010c75890767419257a7
ChihiroHozono <chihiro4434132@gmail.com>
2018-03-12 03:00:40 +0800

    大幅バージョンアップ

--- sumika.php
+++ sumika.php
@@ -3,6 +3,7 @@
 <head>
 	<title>SUMIKA</title>
 	<meta charset="utf-8">
+	<!-- css -->
 	<link rel="stylesheet" type="text/css" href="sumika.css">
 	<!-- フォントオーサム -->
 	<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
@@ -14,84 +15,113 @@
 	  	crossorigin="anonymous">
   	</script>
 </head>
+
 <body>
 
 	<div class="container">
-	<!-- タイトル -->
-		<div class="top">
-			<h3>SUMIKA</h3>
-		</div>
 
-	<!-- スミト -->
-		<div class="humans">
-				<a href="#" class="human"><i class="fas fa-male fa-2x red-human"></i></a>
-				<span class="human"><i class="fas fa-male fa-2x green-human"></i></span>
-				<span class="human"><i class="fas fa-male fa-2x pink-human"></i></span>
-				<span class="human"><i class="fas fa-male fa-2x yellow-human"></i></span>
-				<span class="human"><i class="fas fa-male fa-2x blue-human"></i></span>
-				<span class="human"><i class="fas fa-male fa-2x"></i></span>
-		</div>
+	<!-- 音声ファイル -->
+	<audio id="sound-file" preload="auto">
+		<source src="hanma.wav" type="audio/wav">
+	</audio>
 
-	<!-- 位置の取得 -->
-		<form method="POST" action="sumika.php">
-			<input type="text" name="name">
-			<input type="button" value = "SUMIKA" name="location"
-			onClick='navigator.geolocation.getCurrentPosition(echo_geodata)'>
-			<input type="submit" name="KURASU" value='KURASU'>
-		</form>
+	<header>
+		<!-- ハンバーガー -->
+		<span><i class="fas fa-align-justify fa-2x"></i></span>
+
+
+		<!-- タイトル -->
+	<span class="title">SUMIKA</span>
+	</header>
+
+	<!-- 背景 -->
+	<div class="main">
+		<img class = "background" src="sumika.png">
+		<!-- メインスミカとメインスミト -->
+
+		<i class="fas fa-home fa-4x main-sumika"></i>
+		<i class="fas fa-male fa-1x main-sumito"></i>
 	</div>
 
 
-	<!-- PHP -->
-	<?php
-	// サニタイズ化と表示
-		$name = htmlspecialchars($_POST['name']);
-		//１データベースに接続
-		$dsn='mysql:dbname=sumika;host=localhost';
-		$user='root';
-		$password='';
-		$dbh=new PDO($dsn,$user,$password);
-		$dbh->query('SET NAMES utf8');
+	<!-- スミカを作る -->
+	<h3 class="main-text">スミカを作る</h3>
+	<p class="print-time"></p>
+	<p class="print-latitude"></p>
+	<p class="print-longitude"></p>
+	<a href="#"><i class="fas fa-gavel fa-2x create-sumika"></i></a>
 
-		//２ SQL文を実行する
-		$sql="INSERT INTO `test` (`id`, `name`) VALUES (NULL,?);";
 
-		$data = array($name);
-		$stmt=$dbh -> prepare($sql);
-		$stmt->execute($data);
 
-		// データベースの切断
-		$dbh=null;
+	<!-- スミト -->
 
-	?>
+	<h3 class="main-text">スミトを選ぶ</h3>
+		<div class="humans">
+				<a href="#" class="human"><i class="fas fa-male fa-5x red-human"></i></a>
+				<a href="#" class="human"><i class="fas fa-male fa-5x green-human"></i></a>
+				<a href="#" class="human"><i class="fas fa-male fa-5x pink-human"></i></a>
+				<a href="#" class="human"><i class="fas fa-male fa-5x yellow-human"></i></a>
+				<a href="#" class="human"><i class="fas fa-male fa-5x blue-human"></i></a>
+				<a href="#" class="human"><i class="fas fa-male fa-5x
+				black-human">
+				</i></a>
+		</div>
+
+	<!-- ヒトコト -->
+	<h3 class="main-text">ヒトコト</h3>
+	<input type="text" name="" size="55" placeholder="ヒトコト">
+	</div>
 
 
 
 	<!-- js -->
 	<script type="text/javascript">
+
+		// 日付やジオデータを表示する関数
+
 		function echo_geodata(position){
-			// 空の連想配列
-			var data = [];
 
 			// 日付データ
 			var time = new Date();
-			data['time'] = time;
-			alert(data['time']);
 
 
 			// ジオデータ
-			data['latitude'] = position.coords.latitude;
-			data['longitude'] = position.coords.longitude;
-			alert(data['latitude'])
-			alert(data['longitude'])
+			var latitude = position.coords.latitude;
+			var longitude= position.coords.longitude;
+
+
+			// 日付とジオデータを表示
+			$('.print-time').text(time);
+			$('.print-latitude').text(latitude);
+			$('.print-longitude').text(longitude);
+
+		}
+		// 音声ファイルを再生する関数
+		function sound(){
+			// [ID:sound-file]の音声ファイルを再生[play()]する
+			document.getElementById( 'sound-file' ).play() ;
 		}
 
 
 
-		// JQuery
-		$('.red-human').click(function(){
-			$('.red-human').css('color:red')
+		// JQuery------------------------------------------------
+		// スミカを作るを押した時の動作
+		$('.create-sumika').click(function(){
+			navigator.geolocation.getCurrentPosition(echo_geodata);
+    		$('.main-sumika').animate({opacity: '1'},5000);
+    		sound();
+
+    	});
+
+
+		// スミトをクリック
+		$('.fa-male').click(function(){
+			$(this).css('opacity','1');
+			var color = $(this).css('color');
+			$('.main-sumito').css('color',color).css('opacity','1');
 		});
+
+
 
 	</script>
 </body>
---
+++ sumikia-text.php
@@ -0,0 +1,51 @@
+	<?php
+	// サニタイズ化と表示
+		$name = htmlspecialchars($_POST['name']);
+		//１データベースに接続
+		$dsn='mysql:dbname=sumika;host=localhost;';
+		$user='root';
+		$password='';
+		$dbh=new PDO($dsn,$user,$password);
+		$dbh->query('SET NAMES utf8');
+
+		//２ SQL文を実行する
+		$sql="INSERT INTO `test` (`id`, `name`) VALUES (NULL,?);";
+
+		$data = array($name);
+		$stmt=$dbh -> prepare($sql);
+		$stmt->execute($data);
+
+		// データベースの切断
+		$dbh=null;
+
+	?>
+
+
+		<!-- データの表示 -->
+	<?php
+		// １．データベースに接続する
+		$dsn = 'mysql:dbname=sumika;host=localhost';
+		$user = 'root';
+		$password = '';
+		$dbh = new PDO($dsn, $user, $password);
+		$dbh->query('SET NAMES utf8');
+
+		// ２．SQL文を実行する
+		$sql = 'SELECT * FROM `test`';
+		$stmt = $dbh->prepare($sql);
+		$stmt->execute();
+
+		while (1) {
+		  $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+		  if ($rec == false) {
+		    break;
+		  }
+		  echo $rec['id'] . '<br>';
+		  echo $rec['name'] . '<br>';
+
+		}
+
+		// ３．データベースを切断する
+		$dbh = null;
+
+	?>
